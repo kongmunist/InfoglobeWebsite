f84ff57834b079fa4a4e60cc52f22f61809598d7
Andy Kong <andyking99@gmail.com>
2022-09-15 21:06:20 -0500

    dot now tracks where you click

--- static/anothertable.js
+++ static/anothertable.js
@@ -1,8 +1,10 @@
 // Make the DIV element draggable:
 dragElement(document.getElementById("mydiv"));
-/* dragElement(document.getElementById("mydiv2")); */
 
-var yes = document.getElementById("yes")
+
+// Green/red button that controls the layers
+var button = document.getElementById("editButton")
+
 
 function dragElement(elmnt) {
   var pos1 = 0,
@@ -37,6 +39,11 @@ function dragElement(elmnt) {
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+    // Set the dot's position too if its not invisible
+    if (button.style.display != "none"){
+        moveDotBeside(curFocusedCell)
+    }
   }
 
   function closeDragElement() {
@@ -47,25 +54,58 @@ function dragElement(elmnt) {
 }
 
 
+
+
+
+
+
+
 function createEntry(){
-	elem = document.createElement("div");
+    return createEntryWithText("");
+}
+
+function createEntryWithText(txt){
+  elem = document.createElement("div");
   elem.className = "entry";
   elem.contentEditable = true;
-  /* elem.innerHTML = "Text" */
+
+  elem.onfocus = () => focAndAddDot(event)
+  elem.onblur = () => unfoc(event)
+
+  elem.innerText = txt;
+
   return elem;
 }
 
+function createSublist(){
+  elem = document.createElement("div");
+  elem.className = "entrySublist";
+  elem.contentEditable = true;
+
+  elem.onfocus = () => focAndAddDot(event)
+  elem.onblur = () => unfoc(event)
+
+
+  inDiv = document.createElement("div");
+  inDiv.contentEditable = true;
+  inDiv.className = "innerDiv";
+
+  elem.appendChild(inDiv)
+
+  return elem;
+}
+
+var expose;
 function plusRow(event){
-  let pDiv = event.path[1];
+  let pDiv = getCurrentFocusedTable(event.path);
   let kids = pDiv.children;
   kids[kids.length - 2].before(createEntry());
 }
 
-
-var nao;
 function minusRow(event){
-  let pDiv = event.path[1];
+  let pDiv = getCurrentFocusedTable(event.path)
   let kids = pDiv.children;
+  expose = event.path
 
   let maybeRemove = kids[kids.length-3];
 
@@ -78,29 +118,80 @@ function minusRow(event){
     // Need to remove entry sublist if its empty, otherwise
   	console.log("entrySublist")
   	if (maybeRemove.innerText == ""){
-    	nao = kids
     	maybeRemove.remove();
-      // shrink the entryExpanded too
-      shrink = kids[kids.length-3];
-			shrink.className = "entry"
+        // shrink the entryExpanded too
+        shrink = kids[kids.length-3];
+        shrink.className = "entry"
     }
   }
 }
 
-
-function focAndAddDot(event){
-	let prnt = event.path[0];
-  /* console.log(prnt) */
-
-  // set dot location
-  left = getLeft(prnt)
-	button = document.getElementById("editButton")
-  nao = document.getElementById("editButton");
-
-  button.style.left = left + "px";
-  console.log(button.style.left);
+function addEnToTable(tableID, entry){
+    table = document.getElementById(tableID)
+    kids = table.children
+    kids[kids.length-2].before(entry);
 }
 
+
+
+
+
+
+
+// When a box is highlighted, this function moves the dot to its side and
+// allows us to add/remove layers to the nesting
+var curFocusedCell;
+function focAndAddDot(event){
+    button.style.display = "";
+    clicked = event.path[0];
+
+    moveDotBeside(clicked)
+    curFocusedCell = clicked;
+}
+
+
+// When clicking off the element, stop sticking the mouse to that table
+function unfoc(event){
+    console.log("esbo blur")
+//    button.style.display = "none";
+//    curFocusedCell = null;
+}
+
+function moveDotBeside(elmnt){
+    // set dot location
+    let cellPos = getPos(elmnt)
+    button.style.left = (cellPos.left+clicked.clientWidth + 10) + "px";
+    button.style.top = (cellPos.top) + "px";
+}
+
+function entry2Expanded(entry){
+    if (entry.className != "entry"){
+        return;
+    }
+    // convert entry to entryExpanded
+    entry.className = "entryExpanded"
+
+    // Add a new sublist
+    elem = createSublist();
+    entry.after(elem)
+    }
+
+
+
+
+
+function getCurrentFocusedTable(path){
+    let ind = 0;
+    while (ind < path.length && path[ind].className != "addableDiv"){
+        ind += 1
+    }
+    console.log(path[ind])
+    return path[ind]
+}
+
+
+
+// getting div positions
 function getLeft(elem){
   return getPos(elem).left
 }
@@ -120,3 +211,12 @@ function getPos(elem){
 function blockClick(e){
   e.stopPropagation();
 }
+
+
+//// For testing/dev
+//tableID = "mydiv"
+//addEnToTable(tableID, createEntryWithText("Get residence permit"));
+//addEnToTable(tableID, createEntryWithText("Other stuff"));
+//en = createEntryWithText("Buy a couch");
+//addEnToTable(tableID, en);
+//entry2Expanded(en)
